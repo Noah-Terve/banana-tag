a2828c042b3cd916a4d3180763648d37b1d8e635
rdvaq <rdvaq4@gmail.com>
2024-03-29 20:16:37 -0400

    more archi test

--- src/architecture_testing/playerinput.py
+++ src/architecture_testing/playerinput.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 from erpy import stdio_port_connection
+import sys 
 
 
 def main():
+    sys.stdin = open("/dev/tty")
     inbox, port = stdio_port_connection()
     while True:
         msg = input("Enter Message:")
