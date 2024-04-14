7f7904daf76b34825bf441f481a8ad96130482a8
Noah Tervalon <noahtervalon@gmail.com>
2024-04-14 13:06:18 -0400

    added minor things to server

--- src/server.py
+++ src/server.py
@@ -1,12 +1,14 @@
 from os import environ
 environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
 
-import sys
-from time import sleep
+
 import pygame
 from random import randrange as rr
 from math import sqrt
 from enum import Enum
+import logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='server.log', encoding='utf-8', level=logging.DEBUG)
 
 from erpy import stdio_port_connection
 from term import Atom
@@ -21,11 +23,19 @@ DISPLAY_HEIGHT = 720
 RADIUS = 40
 MOVE_SPEED = 2
 DIRECTIONS = {"a": [-MOVE_SPEED, 0], "s": [0, -MOVE_SPEED], "d": [MOVE_SPEED, 0], "w": [0, MOVE_SPEED]}
+INPUTS = ["a", "w", "s", "d"]
 
 class status(Enum):
     FREE = 0
     TAGGED = 1
     CONTENTION = 2
+
+class choices(Enum):
+    NONE = 0
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+    
 
 # these are status, might want to add one for in contact while they are figuring
 # out who wins
@@ -57,7 +67,9 @@ class player:
         self.pos = pygame.Vector2(rr(RADIUS + 1, DISPLAY_WIDTH - RADIUS + 1), rr(RADIUS + 1, DISPLAY_HEIGHT - RADIUS + 1))
         self.color = make_new_rand_color()
         self.input = []
+        self.choice = choices.NONE
     
+    # it is an error to call this function if a player is in contention.
     def move(self):
         for val in self.input:
             x , y = DIRECTIONS[val]
@@ -73,22 +85,29 @@ class player:
 # progess, but just to have something. This stores coordinates for "players",
 # those are the center of small circles which encompass the full player position
 class game_state:
-    def __init__(self, num_players) -> None:
-        self.num_players = num_players
-        self.players = [0]*num_players
+    def __init__(self) -> None:
+        self.num_players = 0
+        self.players = {} # players is a lookup from name to play struct
+                          # players with the same name will have tags added to
+                          # the end of them to identify
+        
+        
+    def add_player(self, name):
+        keys = self.players.keys()
+        if name in keys:
+            tag = 1
+            while True:
+                if not (name + str(tag)) in keys:
+                    name = name + str(tag)
+                    break
+                tag += 1
+        
+        self.players[name] = player(name)
         # somehow set the starting location of players
-        
-        # for now just do something random
-        i = 0
-        for _ in self.players:
-            # for use with pygame
-            # gives a name, current status, who got you out, position, and color to each player
-            # You'll only need to send out the status and position with updates,
-            # name and color don't change
-            self.players[i] = player("Player " + str(i))
-            i += 1
-        
-        # print(self.players)
+
+
+# global game state to be shared among proceses
+GS = game_state()
 
 def check_collisions(player, players):
     for other_player in players:
@@ -97,14 +116,6 @@ def check_collisions(player, players):
         if dist(player.pos, other_player.pos) <= (RADIUS * 2):
             return True, other_player
     return False, None
-
-def collide(p1, p2):
-    p1.status = status.CONTENTION
-    p2.status = status.CONTENTION
-    # TODO: may have to send updates here, or start some clock attached to the
-    #       players which counts down.. we can figure out how long this needs to
-    #       be from testing the actual latency
-
 
 def move(pos):
     # TODO: This needs to get updated when we decide what the input from the
@@ -120,7 +131,7 @@ def move(pos):
 
 # this is for when the game is starting
 # collect players that want to join
-def start_state(gs):
+def start_state():
     # TODO: while here wait until everyone is ready, and be waiting to accept
     #       people into the room
     
@@ -129,45 +140,31 @@ def start_state(gs):
     pass
 
 
-def game_running(gs):
+def run_game():
     running = True
     update = True
     
-    with open("output.txt", "a") as f:
-        print("In Game running", file=f, flush=True)
     
     while running:
         # TODO: one thing to figure out is when we send updates
         if update:
-            with open("output.txt", "a") as f:
-                print("In update right now", file=f, flush=True)
             # grab updates on what players are doing from erlang
-            i = 0
-            # TODO: this will block until it actually gets 10 messages, need
-            #       to add a timeout 
+            
             for msg in INBOX:
-                with open("output.txt", "a") as f:
-                    print(f"In inbox, iter {i}", file=f, flush=True)
-                
-                if (i == 10):
-                    break
                 
                 if msg == Atom("close"):
                     with open("output.txt", "a") as f:
                         print(f"Closing", file=f, flush=True)
                     return
                 
-                with open("output.txt", "a") as f:
-                    print(f"got {msg}, type: {type(msg)}, {dir(msg)}", file=f, flush=True)
+                logger.info(f"got {msg}, type: {type(msg)}, {dir(msg)}", file=f, flush=True)
 
                 i += 1
                 
             update = False
         
         else:
-            with open("output.txt", "a") as f:
-                print("In else to perform updates right now", file=f, flush=True)
-            for player in gs.players:
+            for player in GS.players:
                 # for every player check if their status should change
                 # should change if:
                 # 1) 2 players are connected
@@ -190,20 +187,18 @@ def game_running(gs):
                     player.got_me_out = None
                     
                 # check collisions
-                collision, other_player = check_collisions(player, gs.players)
+                collision, other_player = check_collisions(player, GS.players)
                 if collision:
-                    collide(player, other_player)
+                    player.status = status.CONTENTION
+                    other_player.status = status.CONTENTION
                 
                 if player.status == status.FREE:
                     player.move()
             update = True
 
 def main():
-    gs = game_state(0)
-    start_state(gs)
-    game_running(gs)
-    with open("output.txt", "a") as f:
-        print(f"Done", file=f, flush=True)
+    start_state()
+    run_game()
 
 
 if __name__ == "__main__":
